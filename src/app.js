dade1caf61e18d679de40547d0e64eee8781d8a0
bboolean <this@is.fake>
2024-01-23 09:31:32 -0500

    Init

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function ButtonUsage() {
+  return <Button variant="contained">Hello world</Button>;
+}
+
+export function App() {
+  return (
+    <div>
+      <Button variant="contained">Hello world</Button>
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
